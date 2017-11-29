a8895bb93011b691d46f1c4a488572f97298dd0a
leeqiang250 <leeqiang250@163.com>
2017-11-29 17:37:53 +0800

    init

---
+++ TestCustomPods/TestCustomPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCustomPods
+//
+//  Created by liqiang on 2017/11/29.
+//  Copyright © 2017年 liqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCustomPods/TestCustomPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCustomPods
+//
+//  Created by liqiang on 2017/11/29.
+//  Copyright © 2017年 liqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

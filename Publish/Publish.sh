1ef95e5a0bc3455e5db059801a37e20669a20b25
leeqiang250 <leeqiang250@163.com>
2018-06-15 16:38:16 +0800

    d

--- Publish/Publish.sh
+++ Publish/Publish.sh
@@ -87,8 +87,7 @@ xcodebuild archive -workspace ${workspace}.xcworkspace -scheme ${targetProject} 
 
 echo "--------------------(生成IPA)--------------------"
 #导出IPA
-xcodebuild -exportArchive -archivePath ${archivePath} -exportPath ${exportPackageName} -exportOptionsPlist ${workspacePath}/Publish/${exportOptions}.plist -exportFormat IPA -exportProvisioningProfile "XC: *"
-
+xcodebuild -exportArchive -archivePath ${archivePath} -exportPath ${exportPackageName} -exportOptionsPlist ${workspacePath}/Publish/${exportOptions}.plist -exportFormat IPA -exportProvisioningProfile "comkcashwalletleeqiangmac"
 
 
 echo "--------------------(移动IPA)--------------------"

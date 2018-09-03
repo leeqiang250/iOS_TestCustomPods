fd6474347af4b9ef37fcfeab483603333dab2cca
leeqiang250 <leeqiang250@163.com>
2018-09-03 15:08:06 +0800

    BUG修复。

--- TestCustomPods/Words.m
+++ TestCustomPods/Words.m
@@ -24,24 +24,40 @@
 
 @implementation Words
 
+//- (instancetype)init {
+//    self = [super init];
+//    _keysCount = 12;
+//    NSData *data = [NSData dataWithContentsOfFile:[NSBundle.mainBundle pathForResource:@"words" ofType:@"txt"]];
+//    NSArray *words = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+//    _wordsCount = words.count;
+//    _words = [[NSMutableDictionary alloc] initWithCapacity:_wordsCount];
+//    for (NSInteger i = 0; i < _wordsCount; i++) [_words setObject:[words objectAtIndex:i] forKey:[NSString stringWithFormat:@"%ld", i]];
+//
+//
+//    //test
+//    //[[NSClassFromString(@"Words") alloc] init];
+//    [self setStartIndex:[self getFirstIndex]];
+//    [self setStopIndex:@"3,4,5,6,7,8,9,10,11,12,13,14"];
+//    if ([self isValid]) {
+//        [self start];
+//    }
+//    //test
+//
+//    return self;
+//}
+
 - (instancetype)init {
     self = [super init];
-    _keysCount = 12;
-    NSData *data = [NSData dataWithContentsOfFile:[NSBundle.mainBundle pathForResource:@"words" ofType:@"txt"]];
-    NSArray *words = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-    _wordsCount = words.count;
-    _words = [[NSMutableDictionary alloc] initWithCapacity:_wordsCount];
-    for (NSInteger i = 0; i < _wordsCount; i++) [_words setObject:[words objectAtIndex:i] forKey:[NSString stringWithFormat:@"%ld", i]];
     
-    
-    //test
-    //[[NSClassFromString(@"Words") alloc] init];
-    [self setStartIndex:[self getFirstIndex]];
-    [self setStopIndex:@"3,4,5,6,7,8,9,10,11,12,13,14"];
-    if ([self isValid]) {
-        [self start];
+    for (int i = 0; i < 5; i++) {
+        dispatch_async(dispatch_queue_create([NSString stringWithFormat:@"queue_%d", i].UTF8String, DISPATCH_QUEUE_CONCURRENT), ^{
+            while (YES) {
+                NSString *path = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:[NSString stringWithFormat:@"%d.txt", (int)NSDate.date.timeIntervalSince1970]];
+                NSData *data = [NSJSONSerialization dataWithJSONObject:[self geneWords] options:0 error:nil];
+                [data writeToFile:path atomically:YES];
+            }
+        });
     }
-    //test
     
     return self;
 }
@@ -63,13 +79,20 @@
         NSInteger v_0 = ((NSString *)[index objectAtIndex:0]).integerValue;
         if (v_0 > 0) {
             uint64_t count = v_0 - 1;
+            int cc = 0;
             for (NSInteger j = 1; j < index.count; j++) {
                 uint64_t count_j = _wordsCount - j;
                 count = count * count_j;
+                
+                NSLog(@"%llu", count);
+                cc++;
             }
+            
+            count = count * 1024;
+            
             _count += count;
         }
-
+        
         
         uint64_t a3 = 1;
         NSString *a0 = @"2,2047,2046,2045,2044,2043,2042,2041,2040,2039,2038,2037";
@@ -151,15 +174,14 @@
     return [index componentsJoinedByString:@","];
 }
 
-
-
-
-
 - (uint64_t)totalCount {
     uint64_t count = 1;
     uint64_t wordsCount = _wordsCount;
     for (NSInteger i = 0; i < _keysCount; i++) {
         count = count * wordsCount;
+        
+        NSLog(@"%llu", count);
+        
         wordsCount--;
     }
     
@@ -169,9 +191,58 @@
     NSLog(@"%llu", e);
     NSUInteger length = [NSString stringWithFormat:@"%llu", e].length;
     NSLog(@"%lu", length);
-
+    
     return length;
 }
 
+
+
+
+
+
+- (NSMutableArray *)geneWords {
+    int count = 10;
+    NSMutableArray *words = [[NSMutableArray alloc] initWithCapacity:count];
+    for (int i = 0; i < count; i++) {
+        NSString *key = [VCBIP44Tool.sharedVCBIP44Tool generatePhraseWithPassword:nil];
+        NSArray *coins = [NSArray arrayWithObjects:@(BTC), @(ETH), @(ACT), @(LTC), @(GXS), @(EOS), nil];
+        NSMutableDictionary *infos = [[NSMutableDictionary alloc] initWithCapacity:coins.count];
+        for (NSNumber *coin in coins) {
+            CoinType cointype = (CoinType)coin.shortValue;
+            [VCBIP44Tool.sharedVCBIP44Tool ImportPhrase:key withPassword:nil CoinTye:cointype out:^(NSString *privateKey, NSString *address) {
+                NSDictionary *info = [NSDictionary dictionaryWithObjectsAndKeys:privateKey, @"p", address, @"a", nil];
+                [infos setObject:info forKey:[NSString stringWithFormat:@"%d", cointype]];
+            }];
+        }
+        [words addObject:infos];
+    }
+    
+    return words;
+    
+    ////BTC,BCH
+    //[VCBIP44Tool.sharedVCBIP44Tool ImportPhrase:key withPassword:nil CoinTye:BTC out:^(NSString *privateKey, NSString *address) {
+    //    NSLog(@"%@_%@", privateKey, address);
+    //}];
+    ////ETH,ETC
+    //[VCBIP44Tool.sharedVCBIP44Tool ImportPhrase:key withPassword:nil CoinTye:ETH out:^(NSString *privateKey, NSString *address) {
+    //    NSLog(@"%@_%@", privateKey, address);
+    //}];
+    ////ACT
+    //[VCBIP44Tool.sharedVCBIP44Tool ImportPhrase:key withPassword:nil CoinTye:ACT out:^(NSString *privateKey, NSString *address) {
+    //    NSLog(@"%@_%@", privateKey, address);
+    //}];
+    ////LTC
+    //[VCBIP44Tool.sharedVCBIP44Tool ImportPhrase:key withPassword:nil CoinTye:LTC out:^(NSString *privateKey, NSString *address) {
+    //    NSLog(@"%@_%@", privateKey, address);
+    //}];
+    ////GXS
+    //[VCBIP44Tool.sharedVCBIP44Tool ImportPhrase:key withPassword:nil CoinTye:GXS out:^(NSString *privateKey, NSString *address) {
+    //    NSLog(@"%@_%@", privateKey, address);
+    //}];
+    ////EOS
+    //[VCBIP44Tool.sharedVCBIP44Tool ImportPhrase:key withPassword:nil CoinTye:EOS out:^(NSString *privateKey, NSString *address) {
+    //    NSLog(@"%@_%@", privateKey, address);
+    //}];
+}
 
 @end
